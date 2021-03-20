622c61a4928d421fa828e9c048de2980cfd64d59
Mauricio Neira <m.neira10@uniandes.edu.co>
2021-03-20 08:24:39 -0500

    prelogin finished

--- front/src/PreLogin/PreLogin.js
+++ front/src/PreLogin/PreLogin.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import UserCard from './UserCard';
-import {ReactComponent as Girl} from './illustrations/girl_circle.svg';
-import {ReactComponent as Man} from './illustrations/men_circle.svg';
-import './PreLogin.css';
+import UserCard from "./UserCard";
+import { ReactComponent as Girl } from "../resources/illustrations/girl_circle.svg";
+import { ReactComponent as Man } from "../resources/illustrations/men_circle.svg";
+import { ReactComponent as Logo } from "../resources/logos/logo_name.svg";
+import "./PreLogin.css";
 
 export default class PreLogin extends Component {
   render() {
     return (
-      <div className='userCardContainer'>
-        <UserCard cardName="Company" cosoPath="/company">
-            <Girl width={200}/>
-        </UserCard>
-
-        <UserCard cardName="Employee" cosoPath="/employee">
-            <Man width={200}/>
-        </UserCard>
-
+      <div>
+        <div className="logo">
+          <Logo height={70} width={150}/>
+        </div>
+        <div className="userCardContainer">
+          <UserCard cardName="Company" cosoPath="/company">
+            <Girl width={200} />
+          </UserCard>   
+          <UserCard cardName="Employee" cosoPath="/employee">
+            <Man width={200} />
+          </UserCard>
+        </div>
       </div>
     );
   }
