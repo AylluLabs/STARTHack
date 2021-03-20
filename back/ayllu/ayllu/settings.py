a2cbff8e91198f053521c1a348a8e6ccf99db393
Mauricio Neira <m.neira10@uniandes.edu.co>
2021-03-19 19:45:43 -0500

    hello world wellbeing view

--- back/ayllu/ayllu/settings.py
+++ back/ayllu/ayllu/settings.py
@@ -73,12 +73,19 @@ WSGI_APPLICATION = 'ayllu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ayllu',
+        'USER': 'rabbitholeadmin@rabbitholedb',
+        'PASSWORD' : "r7pQ3q-q'3^shLBL",
+        'HOST': 'rabbitholedb.postgres.database.azure.com',
+        'PORT' : '5432',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
+
 
 
 # Password validation
