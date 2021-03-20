103520da46c7c154b76d534d995de21560a914b2
Alejandro Lozada <a.lozada10@uniandes.edu.co>
2021-03-19 20:44:52 -0500

    questions db ready

--- back/questions/get_questions.py
+++ back/questions/get_questions.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import pandas as pd
+
 ques_importance = {"I easily adapt to day-to-day changes of my life and manage my responsibilities well.": 0.911,
                    "I care for things that are important to me, not what is important to others.": 0.868,
                    "I feel I am a sensible person.": 0.862,
@@ -19,20 +21,24 @@ ques_importance = {"I easily adapt to day-to-day changes of my life and manage m
                    "I love to spend time with my teammates.": 0.783,
                    "I can freely share my problems with my colleagues.": 0.782,
                    "My day-to-day activities contribute towards the benefits of my team.": 0.768,
-                   "I am quite satisfied with my job.":0.862,
-                   "I enjoy meaningful work":0.825,
-                   "I attach lots of value to my work.":0.817,
-                   "My work achievement often acts as a source of motivation.": 0.806
-
+                   "I am quite satisfied with my job.": 0.862,
+                   "I enjoy meaningful work": 0.825,
+                   "I attach lots of value to my work.": 0.817,
+                   "My work achievement often acts as a source of motivation.": 0.806,
+                   "My workplace is very conducive.": 0.778,
+                   "My job provides ample scope for career growth.": 0.763,
+                   "I used to maintain a balance between work and home life": 0.702,
+                   "My employer does care a lot about their employees.": 0.667,
+                   "My work offers challenges to advance my skills.": 0.582,
+                   "Mostly I feel happy.": 0.825,
+                   "I am an optimistic person.": 0.777,
+                   "I feel good about myself.": 0.722,
+                   "My life is mostly sorrowful. *": 0.593
                    }
 
-
-EWB45:asdasdasd
-EWB47:asdasdad
-EWB48:
-EWB41:
-EWB37: My workplace is very conducive. 0.778
-EWB14: My job provides ample scope for career growth. 0.763
-EWB28: I used to maintain a balance between work and home life 0.702
-EWB43: My employer does care a lot about their employees. 0.667
-EWB25: My work offers challenges to advance my skills. 0.582
+questions_df = pd.DataFrame(
+    data={'question_text': list(ques_importance.keys()), 'importance': list(ques_importance.values())})
+questions_df["reversed"] = [False] * questions_df.shape[0]
+questions_df.loc[questions_df.question_text.str.contains(r"*", regex=False), "reversed"] = True
+questions_df["question_text"] = questions_df["question_text"].str.replace("*", "")
+questions_df.to_csv("questions_importance.csv", index=False)
