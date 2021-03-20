849f99e43f8ae583d6e9e1de303a9850ced4c7c7
Mauricio Neira <m.neira10@uniandes.edu.co>
2021-03-20 08:51:25 -0500

    basic navbar

--- front/src/Employee/Employee.js
+++ front/src/Employee/Employee.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from './navbar/Navbar'
 
 export default class Employee extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 Da employee
             </div>
         )
