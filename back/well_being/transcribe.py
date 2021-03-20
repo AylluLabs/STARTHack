98296592c9b427a46c4c5d21f03f28f829190650
Alejandro Lozada <a.lozada10@uniandes.edu.co>
2021-03-20 01:00:12 -0500

    Voice to text almost done

--- back/well_being/transcribe.py
+++ back/well_being/transcribe.py
@@ -1,5 +1,7 @@
 import time
 import boto3
+import credentials
+import requests
 
 
 def transcribe_file(job_name, file_uri, transcribe_client):
@@ -9,7 +11,7 @@ def transcribe_file(job_name, file_uri, transcribe_client):
         MediaFormat='wav',
         LanguageCode='en-US'
     )
-
+    print("start")
     max_tries = 60
     while max_tries > 0:
         max_tries -= 1
@@ -18,9 +20,11 @@ def transcribe_file(job_name, file_uri, transcribe_client):
         if job_status in ['COMPLETED', 'FAILED']:
             print(f"Job {job_name} is {job_status}.")
             if job_status == 'COMPLETED':
-                print(
+                req = job['TranscriptionJob']['Transcript']['TranscriptFileUri']
+                return req
+                '''print(
                     f"Download the transcript from\n"
-                    f"\t{job['TranscriptionJob']['Transcript']['TranscriptFileUri']}.")
+                    f"\t{job['TranscriptionJob']['Transcript']['TranscriptFileUri']}.")'''
             break
         else:
             print(f"Waiting for {job_name}. Current status is {job_status}.")
@@ -28,8 +32,17 @@ def transcribe_file(job_name, file_uri, transcribe_client):
 
 
 def transcribe():
-    transcribe_client = boto3.client('transcribe')
-    file_uri = 's3://test-transcribe/answer2.wav'
-    transcribe_file('transcribe', file_uri, transcribe_client)
+    transcribe_client = boto3.client('transcribe',
+                                     aws_access_key_id=credentials.AWSAccessKeyId,
+                                     aws_secret_access_key=credentials.AWSSecretKey,
+                                     region_name='us-east-2'
+                                     # aws_session_token=SESSION_TOKEN
+                                     )
+    print("connected")
+    file_uri = 'https://ayllu.s3.us-east-2.amazonaws.com/test.wav'
+    return transcribe_file('transcribe', file_uri, transcribe_client)
 
 
+url = transcribe()
+response = requests.get(url, verify=True)
+print(response.text)
