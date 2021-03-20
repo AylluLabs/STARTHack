09847b89a028657b307c38ef9cc29dc7c7f48fa1
Alejandro Lozada <a.lozada10@uniandes.edu.co>
2021-03-19 23:07:05 -0500

    process poll endpoint done

--- back/ayllu/wellbeing/views.py
+++ back/ayllu/wellbeing/views.py
@@ -4,6 +4,7 @@ from .models import WellbeingQuestion
 import json
 from django.forms.models import model_to_dict
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("hello world")
@@ -13,8 +14,9 @@ def getSampleQuestions(request):
     questions = WellbeingQuestion.objects.all()
     sample = np.random.choice(questions, 3)
     sample = [model_to_dict(x) for x in sample]
+    content_type = "application/json"
 
-    return HttpResponse(json.dumps({"data": list(sample)}))
+    return HttpResponse(json.dumps({"data": list(sample)}), content_type=content_type)
 
 
 def processAudio(request):
@@ -23,5 +25,19 @@ def processAudio(request):
 
 
 def processAnsweredPoll(request):
+    body_unicode = request.body.decode('utf-8')
+    resp = json.loads(body_unicode)
+
+    scores = [i["qscore"] for i in resp["poll"]]
+    id_questions = [i["idQuestion"] for i in resp["poll"]]
+    weights = []
+    for key in id_questions:
+        question = WellbeingQuestion.objects.get(pk=key)
+        if question.weight_reversed:
+            weight = 11 - question.weight
+        else:
+            weight = question.weight
+        weights.append(weight)
+    wellbeing_score = sum(np.array(weights) * np.array(scores)) / sum(weights)
     # TODO
-    return HttpResponse('processAnsweredPoll')
+    return HttpResponse(json.dumps({"wellbeing_score": wellbeing_score}))
